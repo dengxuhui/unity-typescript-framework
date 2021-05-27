d0c36a3dec040b3b5992cdb3c48b940292b47e7c
dengxuhui <419270388@qq.com>
2021-05-27 14:17:01 +0800

    打包设置

--- UnityTypescriptFramework/Assets/Scripts/UnityTs/Js/JsLoader.cs
+++ UnityTypescriptFramework/Assets/Scripts/UnityTs/Js/JsLoader.cs
@@ -27,16 +27,22 @@ namespace UnityTs.Js
 
         public string ReadFile(string filepath, out string debugpath)
         {
-#if UNITY_EDITOR
+// #if UNITY_EDITOR
+//             var dir = Path.Combine(Application.dataPath, "AssetsPackage/Js");
+//             var jsPath = Path.Combine(dir, filepath);
+//             var txt = File.ReadAllText(jsPath);
+//             debugpath = jsPath.Replace("/", "\\");
+//
+//             return txt;
+// #else
+//         //TODO 正式环境加载JS的方式
+// #endif
             var dir = Path.Combine(Application.dataPath, "AssetsPackage/Js");
             var jsPath = Path.Combine(dir, filepath);
             var txt = File.ReadAllText(jsPath);
             debugpath = jsPath.Replace("/", "\\");
 
             return txt;
-#else
-        //TODO 正式环境加载JS的方式
-#endif
         }
     }
 }
