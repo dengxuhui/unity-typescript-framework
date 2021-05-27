7717504d787c443023d00d27b54e87670a808ff2
dengxuhui <419270388@qq.com>
2021-05-27 23:20:00 +0800

    初始化渠道及版本号逻辑

--- UnityTypescriptFramework/Assets/Scripts/GameLaunch.cs
+++ UnityTypescriptFramework/Assets/Scripts/GameLaunch.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
+using AssetBundles;
 using CS.Framework.Conf;
+using GameChannel;
 using UnityEngine;
 
 namespace CS
@@ -23,12 +25,59 @@ namespace CS
             Application.targetFrameRate = 60;
             //js调试器模式
             Config.JsDebugger = jsDebugger;
-            
+
             LoggerHelper.Instance.Startup();
-            
-            var start = DateTime.Now;
+
+            yield return InitAppVersion();
+
+            yield return InitChannel(); 
+
+            //ab管理器启动
+            yield return AssetBundleManager.Instance.Initialize();
             //js管理器启动
             JsManager.Instance.StartGame();
+            yield break;
+        }
+
+        //初始化app版本
+        IEnumerator InitAppVersion()
+        {
+            var appVersionRequest = AssetBundleManager.Instance.RequestAssetFileAsync(BuildUtils.AppVersionFileName);
+            yield return appVersionRequest;
+            var streamingAppVersion = appVersionRequest.text;
+            appVersionRequest.Dispose();
+
+            var appVersionPath = AssetBundleUtility.GetPersistentDataPath(BuildUtils.AppVersionFileName);
+            var persistentAppVersion = GameUtility.SafeReadAllText(appVersionPath);
+            Logger.Log($"streamingAppVersion = {streamingAppVersion}, persistentAppVersion = {persistentAppVersion}");
+            // 如果persistent目录版本比streamingAssets目录app版本低，说明是大版本覆盖安装，清理过时的缓存
+            if (!string.IsNullOrEmpty(persistentAppVersion) &&
+                BuildUtils.CheckIsNewVersion(persistentAppVersion, streamingAppVersion))
+            {
+                var path = AssetBundleUtility.GetPersistentDataPath();
+                GameUtility.SafeDeleteDir(path);
+            }
+
+            GameUtility.SafeWriteAllText(appVersionPath, streamingAppVersion);
+            ChannelManager.instance.appVersion = streamingAppVersion;
+            yield break;
+        }
+
+        //初始渠道
+        IEnumerator InitChannel()
+        {
+#if UNITY_EDITOR
+            if (AssetBundleConfig.IsEditorMode)
+            {
+                yield break;
+            }
+#endif
+            var channelNameRequest = AssetBundleManager.Instance.RequestAssetFileAsync(BuildUtils.ChannelNameFileName);
+            yield return channelNameRequest;
+            var channelName = channelNameRequest.text;
+            channelNameRequest.Dispose();
+            ChannelManager.instance.Init(channelName);
+            Logger.Log($"channelName = {channelName}");
             yield break;
         }
     }
