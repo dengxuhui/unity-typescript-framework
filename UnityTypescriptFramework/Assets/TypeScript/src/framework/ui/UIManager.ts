47ac8e00da6e48fa24a7f48d9cb819e1718e762b
dengxuhui <419270388@qq.com>
2021-05-29 15:32:35 +0800

    层级定义

--- UnityTypescriptFramework/Assets/TypeScript/src/framework/ui/UIManager.ts
+++ UnityTypescriptFramework/Assets/TypeScript/src/framework/ui/UIManager.ts
@@ -1,18 +1,28 @@
 import EventDispatcher from "../utils/EventDispatcher";
 import {ISingleton} from "../interface/ISingleton";
+import {UILayers} from "./UILayers";
+import {UnityEngine} from "csharp";
 
 /**
  * ui管理器系统：提供UI操作，UI层级管理
  */
-export default class UIManager extends EventDispatcher implements ISingleton{
-    public static Instance:UIManager = new UIManager();
+export default class UIManager extends EventDispatcher implements ISingleton {
+    public static Instance: UIManager = new UIManager();
     //ui场景根目录
     static UIRootPath: string = "UIRoot";
     //事件路径
     static EventSystemPath: string = "EventSystem";
-
+    //camera路径
+    static UICameraPath = UIManager.UIRootPath + "/UICamera";
+    //分辨率
+    static Resolution = new UnityEngine.Vector2(750, 1334);
+    //窗口最大可使用的相对order_in_layer
+    static MaxOrderPerWindow:number = 10;
+    
+    
     //所有窗口记录
-    allWindows:Array<any>;
+    allWindows: Array<any>;
+
     /**
      * 密封构造函数
      */
@@ -24,6 +34,6 @@ export default class UIManager extends EventDispatcher implements ISingleton{
      * 初始化
      */
     public initialize(): void {
-        
+        UILayers.set();
     }
 }
