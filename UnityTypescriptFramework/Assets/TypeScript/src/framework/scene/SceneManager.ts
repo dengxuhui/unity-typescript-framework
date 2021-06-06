8d8fa7922429e02c9747d35bae1cf0ff266a4aac
dengxuhui <419270388@qq.com>
2021-06-06 23:23:25 +0800

    场景管理器

---
+++ UnityTypescriptFramework/Assets/TypeScript/src/framework/scene/SceneManager.ts
@@ -0,0 +1,57 @@
+import {ISingleton} from "../interface/ISingleton";
+import {BaseScene} from "./base/BaseScene";
+import UIManager from "../ui/UIManager";
+import IDestroyable from "../interface/IDestroyable";
+import {TimerMgr} from "../utils/timer/Timer";
+
+export class SceneManager implements ISingleton, IDestroyable {
+    /**
+     * 当前场景
+     */
+    private _currentScene: BaseScene;
+    /**
+     * 是否加载中
+     */
+    private _busing: boolean;
+    /**
+     * 场景map
+     */
+    private _sceneMap: Map<string, BaseScene>;
+
+    public initialize(): void {
+        this._sceneMap = new Map<string, BaseScene>();
+        this._busing = false;
+        this._currentScene = null;
+    }
+
+    public destroy(): void {
+        this._currentScene = null;
+        if (this._sceneMap) {
+            this._sceneMap.forEach((scene, name) => {
+                scene && scene.destroy();
+            });
+            this._sceneMap = null;
+        }
+    }
+
+    /**
+     * 切换场景
+     * @param sceneConfig
+     */
+    public switchScene(sceneConfig) {
+        if (this._busing) {
+            return;
+        }
+        if (this._currentScene && this._currentScene.getConfig() == sceneConfig) {
+            return;
+        }
+        this._busing = true;
+        // this.innerSwitchScene();
+    }
+    
+    private innerSwitchScene(){
+        TimerMgr.timer.frameOnce(1,this,()=>{
+            
+        },null);
+    }
+}
---
+++ UnityTypescriptFramework/Assets/TypeScript/src/framework/scene/base/BaseScene.ts
@@ -0,0 +1,15 @@
+import IDestroyable from "../../interface/IDestroyable";
+
+export class BaseScene implements IDestroyable{
+    constructor() {
+        
+    }
+    
+    public destroy(): void {
+        
+    }
+    
+    public getConfig(){
+        
+    }
+}
