1eb50568be4d17cae251e0fb18d06771119cdf4c
dengxuhui <419270388@qq.com>
2021-06-06 19:46:50 +0800

    资源加载逻辑

--- UnityTypescriptFramework/Assets/TypeScript/src/framework/resource/ResourceManager.ts
+++ UnityTypescriptFramework/Assets/TypeScript/src/framework/resource/ResourceManager.ts
@@ -1,7 +1,8 @@
 import {ISingleton} from "../interface/ISingleton";
-import {AssetBundles, System} from "csharp";
+import {AssetBundles, CS, System} from "csharp";
 import Handler from "../utils/Handler";
 import {TimerMgr} from "../utils/timer/Timer";
+import {string} from "../utils/StringUtil";
 
 export class ResourceManager implements ISingleton {
     public static Instance: ResourceManager = new ResourceManager();
@@ -9,34 +10,70 @@ export class ResourceManager implements ISingleton {
      * cs侧资源接口
      */
     private _api: AssetBundles.AssetBundleManager;
-    private _requestHandler: Map<AssetBundles.BaseAssetAsyncLoader, Handler>;
+    private _requestAssetsHandler: Map<AssetBundles.BaseAssetAsyncLoader, Handler>;
+    private _requestABHandler: Map<AssetBundles.BaseAssetAsyncLoader, Handler>;
 
     private constructor() {
     }
 
     private onUpdate() {
-        if (this._requestHandler.size <= 0) {
-            return;
+        if (this._requestAssetsHandler.size > 0) {
+            this._requestAssetsHandler.forEach((handler, loader) => {
+                if (loader.isDone) {
+                    handler && handler.runWith(loader.asset);
+                    loader.Dispose();
+                    this._requestAssetsHandler.delete(loader);
+                }
+            });
         }
 
-        this._requestHandler.forEach((handler, loader) => {
-            if (loader.isDone) {
-                handler && handler.runWith(loader.asset);
-                loader.Dispose();
-                this._requestHandler.delete(loader);
-            }
-        });
+        if (this._requestABHandler.size > 0) {
+            this._requestABHandler.forEach((handler, loader) => {
+                if (loader.isDone) {
+                    handler && handler.runWith(true);
+                    loader.Dispose();
+                    this._requestABHandler.delete(loader);
+                }
+            });
+        }
     }
 
 
     public initialize(): void {
         this._api = AssetBundles.AssetBundleManager.Instance;
-        this._requestHandler = new Map<AssetBundles.BaseAssetAsyncLoader, Handler>();
-        TimerMgr.timer.frameLoop(1, this, this.onUpdate, null, true);
+        this._requestAssetsHandler = new Map<AssetBundles.BaseAssetAsyncLoader, Handler>();
+        this._requestABHandler = new Map<AssetBundles.BaseAssetAsyncLoader, Handler>();
+        TimerMgr.timer.frameLoop(2, this, this.onUpdate, null, true);
     }
 
-    public loadAssetAsync(path: string, res_type: System.Type, callback: Handler) {
+    /**
+     * 异步加载资源
+     * @param path
+     * @param res_type
+     * @param callback
+     */
+    public loadAssetAsync(path: string, res_type: System.Type, callback: Handler): boolean {
+        if (string.IsNullOrEmpty(path)) {
+            CS.Logger.LogError("ResourceManager::loadAssetAsync params error,path is empty");
+            return false;
+        }
         let request = this._api.LoadAssetAsync(path, res_type);
-        this._requestHandler.set(request, callback);
+        this._requestAssetsHandler.set(request, callback);
+        return true;
+    }
+
+    /**
+     * 异步加载AB包
+     * @param path
+     * @param callBack
+     */
+    public loadAssetBundleAsync(path: string, callBack: Handler) {
+        if (string.IsNullOrEmpty(path)) {
+            CS.Logger.LogError("ResourceManager::loadAssetBundleAsync params error,path is empty");
+            return;
+        }
+        let request = this._api.LoadAssetBundleAsync(path);
+        this._requestABHandler.set(request, callBack);
+        return true;
     }
 }
