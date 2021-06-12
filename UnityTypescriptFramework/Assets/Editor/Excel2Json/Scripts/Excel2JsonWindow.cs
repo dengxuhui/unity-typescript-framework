a9f36af731a1453d62980477bce8929a4f85338d
dengxuhui <419270388@qq.com>
2021-06-12 13:48:38 +0800

    导出工具

--- UnityTypescriptFramework/Assets/Editor/Excel2Json/Scripts/Excel2JsonWindow.cs
+++ UnityTypescriptFramework/Assets/Editor/Excel2Json/Scripts/Excel2JsonWindow.cs
@@ -1,8 +1,16 @@
+using System.IO;
+using Excel2Json.func;
 using UnityEditor;
 using UnityEngine;
 
 namespace Excel2Json
 {
+    enum FileType
+    {
+        File,
+        Directory,
+    }
+
     /// <summary>
     /// excel转json unity中的操作界面
     /// </summary>
@@ -20,7 +28,10 @@ namespace Excel2Json
         #region 属性
 
         //xlsx路径
-        private static string xlsxFolder = string.Empty;
+        private static string xlsxFolderOrPath = string.Empty;
+
+        //生成接口
+        private static bool _exportInterface = false;
 
         #endregion
 
@@ -28,25 +39,55 @@ namespace Excel2Json
         {
             #region 转json
 
+            EditorGUILayout.LabelField("---------------1.excel转json工具---------------");
+            EditorGUILayout.LabelField("说明：选择Excel目录或文件夹导出json文件，导出路径需要在Excel2JsonRules.asset中配置");
             GUILayout.BeginHorizontal();
 
-            GUILayout.Label("选择xlsx文件或目录：");
-            xlsxFolder = GUILayout.TextField(xlsxFolder);
-            if (GUILayout.Button("文件夹"))
+            GUILayout.Label("选择xlsx文件或目录：", GUILayout.MaxWidth(120));
+            xlsxFolderOrPath = GUILayout.TextField(xlsxFolderOrPath, GUILayout.MaxWidth(360));
+            if (GUILayout.Button("文件夹", GUILayout.MaxWidth((80))))
             {
+                SelectXlsxFolder(FileType.Directory);
             }
 
-            if (GUILayout.Button("文件"))
+            if (GUILayout.Button("文件", GUILayout.MaxWidth(80)))
             {
+                SelectXlsxFolder(FileType.File);
             }
 
             GUILayout.EndHorizontal();
+            _exportInterface = EditorGUILayout.ToggleLeft("是否生成ts接口", _exportInterface);
+            if (GUILayout.Button("excel转json"))
+            {
+                Debug.Log("Excel2Json:::start export json");
+                FuncExcel2Json.Export(xlsxFolderOrPath, _exportInterface);
+                Debug.Log("Excel2Json:::complete export json");
+            }
 
             #endregion
 
             #region 转typescript接口
 
             #endregion
+        }
+        
+        
+
+        private void SelectXlsxFolder(FileType type)
+        {
+            string selectXlsxPath = string.Empty;
+            if (type == FileType.File)
+            {
+                selectXlsxPath =
+                    EditorUtility.OpenFilePanelWithFilters("Select xlsx file", xlsxFolderOrPath,
+                        new[] {"xlsx", "xlsx"});
+            }
+            else if (type == FileType.Directory)
+            {
+                selectXlsxPath = EditorUtility.OpenFolderPanel("Select xlsx folder", "", "");
+            }
+
+            xlsxFolderOrPath = selectXlsxPath;
         }
 
         #endregion
--- UnityTypescriptFramework/Assets/Editor/Excel2Json/Scripts/func/FuncExcel2Json.cs
+++ UnityTypescriptFramework/Assets/Editor/Excel2Json/Scripts/func/FuncExcel2Json.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using System.Data;
 using System.IO;
+using Excel;
+using UnityEditor;
+using UnityEngine;
 
 namespace Excel2Json.func
 {
@@ -7,23 +12,37 @@ namespace Excel2Json.func
     /// </summary>
     public static class FuncExcel2Json
     {
-
         /**
          * 单路径导出
          */
-        public static void Export(string path)
+        public static void Export(string path, bool exportInterface)
         {
-            
+            if (string.IsNullOrEmpty(path) || (!File.Exists(path) && !Directory.Exists(path)))
+            {
+                EditorUtility.DisplayDialog("error", $"path error ,not exist=>{path}", "OK");
+                return;
+            }
+            Dictionary<string,DataSet> dataSetDic = new Dictionary<string, DataSet>();
+            if (File.Exists(path) && Path.GetExtension(path) == ".xlsx")
+            {
+                CollectXlsx(path);
+            }
         }
 
-        public static void Export(string[] pathArray)
+        private static void CollectXlsx(string path)
         {
-            
-        }
+            using (var stream = File.Open(path,FileMode.Open,FileAccess.Read))
+            {
+                var excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                if (!string.IsNullOrEmpty(excelReader.ExceptionMessage))
+                {
+                    Debug.LogError($"excel read fail,path=>{path}");
+                    return;
+                }
 
-        public static void Export(DirectoryInfo directoryInfo)
-        {
-            
+                var dataSet = excelReader.AsDataSet();
+                var tableCount = dataSet.Tables.Count;
+            }
         }
     }
 }
